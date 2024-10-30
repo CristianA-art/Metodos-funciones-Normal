73a3a721171a3cc3e20215f535c729eff3bec405
USER <USER@LAPTOP-MAI9O57L>
2024-10-30 08:01:39 -0700

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Metodos_funciones_Normal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MostrarMensaje("Mensaje Desde el metodo...1");
+            MostrarMensaje("Mensaje Desde el metodo...2");
+
+            Sumar(2, 5);
+        }
+
+        static void MostrarMensaje(string mensaje)
+        {
+            Console.WriteLine(mensaje);
+        }
+
+        static void Sumar(int primerNumero, int segundoNumnero)
+        {
+            int total = primerNumero + segundoNumnero;
+            MostrarMensaje($"El resultado de la suma de {primerNumero} Y {segundoNumnero"total.ToString());
+        }
+    }
+}
